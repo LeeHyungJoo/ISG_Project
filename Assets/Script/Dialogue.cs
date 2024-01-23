f77e95184109f20e5573d8ae0886a5047dda761b
Yodle_94 <sj_artist@naver.com>
2024-01-23 19:36:12 +0900

    스크립트 생성

---
+++ Assets/Script/Dialogue.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue
+{
+    public int Seq { get; set; }
+    public string Context { get; set; }
+}
