68c4cc7f4891d51886fe8f54c8630737243b970d
0421cjy <cherryofsnow@gmail.com>
2024-06-01 23:30:03 +0900

    Korean Font added

--- Assets/Script/DialogueController.cs
+++ Assets/Script/DialogueController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DialogueController : MonoBehaviour
 {
     public DialogueListView dialogueView;
+    public Dialog dialogDB;
     public List<Dialogue> diaglueList { get; set; }
 
     //Dialogue View 는 Canvas랑 종속적으로
@@ -12,9 +13,9 @@ public class DialogueController : MonoBehaviour
     {
         Debug.Log("Dialogue Controller Init");
         //임시 : dialogueList 는 User 에서 저장하고 있는 것을. 로드.
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 5; i++)
         {
-            diaglueList.Add(new Dialogue() { SenderName = "Anonymous", Seq = i, Context = $"Test {i}" });
+            diaglueList.Add(new Dialogue() { SenderName = dialogDB.Entities[i].name, Seq = dialogDB.Entities[i].index, Context = dialogDB.Entities[i].dialog });
         }
     }
 
